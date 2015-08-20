27076a99548dd51275129655a53a4f4db8feba01
Paul Stanfield <paulstanfield@Pauls-iMac.local>
2015-08-20 14:54:29 +0100

    websocket fix, cb pic, background colors

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -31,7 +31,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Thankyou, Your message has been sent. </strong>");
+                        .append("<strong>Thankyou, Your message has been sent succesfully to Simulity.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
